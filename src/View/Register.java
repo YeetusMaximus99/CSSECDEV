6471dd779b867337af138c6c78686093688b0abd
YeetusMaximus99 <77537993+YeetusMaximus99@users.noreply.github.com>
2023-02-24 19:38:02 +0800

    Resolved Commit issue

--- src/View/Register.java
+++ src/View/Register.java
@@ -124,17 +124,16 @@ public class Register extends javax.swing.JPanel {
         boolean matchFound = matcher.find();
         
         if(matchFound){
-        frame.registerAction(usernameFld.getText(), passwordFld.getText(), confpassFld.getText());
+        
             if(passwordFld.getText().equals(confpassFld.getText())){
+                byte[] salt = frame.main.sqlite.newSalt();
+                frame.registerAction(usernameFld.getText(), passwordFld.getText(), confpassFld.getText(),salt);
                 frame.loginNav();
             }
             else{
                  showMessageDialog(null,"Password and Confirm Password Do not Match!");
             }
         
-        byte[] salt = frame.main.sqlite.newSalt();
-        frame.registerAction(usernameFld.getText(), passwordFld.getText(), confpassFld.getText(),salt);
-        frame.loginNav();
         }
         else{
            //insert gui warning code error here
